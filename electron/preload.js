a2272410da0c867dfd781124cfcd379c75f8822a
owellandry <owellandry@users.noreply.github.com>
2026-04-14 00:36:32 +0000

    style(electron): quitar comentarios en preload

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,3 @@
 import { contextBridge } from 'electron';
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electron', {
-  // You can add methods here later, e.g.:
-  // getAppVersion: () => ipcRenderer.invoke('get-app-version')
-});
+contextBridge.exposeInMainWorld('electron', {});
